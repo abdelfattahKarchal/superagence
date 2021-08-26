c82f61c4f22e55cc05641be9b5297e86698c299b
Abdelfattah <abdelfattah59@gmail.com>
2021-08-26 18:10:47 +0100

    feat: ajout interaction js (symfony encore (webpack), yarn)

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+// assets/js/app.js
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+let $ = require('jquery');
+import '../styles/app.css';
+
+require ('select2');
+
+
+
+$('select').select2({
+    theme: "classic"
+});
+
+let $contactButton = $('#contactButton');
+$contactButton.on('click', e => {
+    e.preventDefault()
+    $contactButton.slideUp();
+    $('#contactForm').slideDown();
+});
+// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
+// import $ from 'jquery';
+
+
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
